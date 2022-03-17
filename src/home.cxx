ebc3f5684fc73349dd60ace6407d87d2a1a2c048
mnmustafa1109 <mnmustafa1109@gmail.com>
2022-03-17 14:28:24 +0500

    removed sf on everyline

--- src/home.cxx
+++ src/home.cxx
@@ -2,18 +2,20 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <iostream>
 
+using namespace sf;
+
 void home() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(600, 600), "Hello From SFML");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Magenta);
 
     while (window.isOpen()) {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed) {
                 window.close();
+            }
         }
-
         window.clear();
         window.draw(shape);
         window.display();
